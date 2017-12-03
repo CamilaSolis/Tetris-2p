ac6b561d7fadee08e6a088a66b3835a3fbfaa0d0
Camila Solis <camila.s.diseno@gmail.com>
2017-12-03 14:57:11 -0300

    transform code

--- tetris.js
+++ tetris.js
@@ -120,85 +120,12 @@ function merge(arena, player) {
     });
 }
 
-function playerDrop () {
-    player.pos.y++;
-    if (collide(arena, player)) {
-        player.pos.y--;
-        merge(arena, player);
-        playerReset();
-        arenaSweep();
-        updateScore();
-    }
-    dropCounter = 0;
-}
-
-function playerMove(dir){
-    player.pos.x += dir;
-    if (collide(arena, player)) {
-        player.pos.x -= dir;
-    }
-}
-
-function playerReset() {
-    const pieces = 'ILJOTSZ';
-    player.matrix = createPiece(pieces[pieces.length * Math.random() | 0]);
-    player.pos.y = 0;
-    player.pos.x = (arena[0].length / 2 | 0 ) -
-                    (player.matrix[0].length / 2 | 0);
-    if (collide(arena, player)) {
-        arena.forEach(row => row.fill(0));
-        player.score = 0;
-        updateScore();
-    }
-}
-
-function playerRotate(dir) {
-    const pos = player.pos.x;
-    let offset = 1;
-    rotate(player.matrix, dir);
-    while (collide(arena, player)){
-        player.pos.x += offset;
-        offset = -(offset + (offset > 0 ? 1 : -1));
-        if (offset > player.matrix[0].length) {
-            rotate(player.matrix, -dir);
-            player.pos.x = pos;
-            return;
-        }
-    }
-}
-
-function rotate(matrix, dir) {
-    for (let y = 0; y < matrix.length; ++y) {
-        for (let x = 0; x < y; ++x){
-            [
-                matrix[x][y],
-                matrix[y][x],
-
-            ] = [
-                matrix[y][x],
-                matrix[x][y],
-            ];
-        }
-    }
-    if (dir > 0){
-        matrix.forEach(row => row.reverse());
-    } else {
-        matrix.reverse();
-    }
-}
-
-let dropCounter = 0;
-let dropInterval = 1000;
-
 let lastTime = 0;
 function update(time = 0) {
     const deltaTime = time - lastTime;
     lastTime = time;
 
-    dropCounter += deltaTime;
-    if (dropCounter > dropInterval) {
-        playerDrop();
-    }
+    player.update(deltaTime);
 
     draw();
     requestAnimationFrame(update);
@@ -221,25 +148,20 @@ const colors = [
 
 const arena = createMatrix(12, 20);
 
-
-const player = {
-    pos: {x: 0, y: 0},
-    matrix: null,
-    score: 0,
-}
+const player = new Player;
 
 document.addEventListener('keydown', event => {
     if (event.keyCode === 37) {
-        playerMove(-1);
+        player.move(-1);
     } else if (event.keyCode === 39) {
-        playerMove(1);
+        player.move(1);
     } else if (event.keyCode === 40) {
-        playerDrop();
+        player.drop();
     } else if (event.keyCode === 38) {
-        playerRotate(1);
+        player.rotate(1);
     }
 });
 
-playerReset();
+player.reset();
 updateScore();
 update();
