02b7221ed05002855952ff07552afe618b9307a9
Camila Solis <camila.s.diseno@gmail.com>
2017-12-18 18:19:20 -0300

    push html version and react version

--- tetris-rx/src/player.js
+++ tetris-rx/src/player.js
@@ -1,11 +1,55 @@
-class Player
-{
-    constructor(tetris)
-    {
+function createPiece(type) {
+    if (type === 'T') {
+        return [
+                [0, 0, 0],
+                [1, 1, 1],
+                [0, 1, 0],
+        ];
+    } else if (type === 'O'){
+        return [
+            [2, 2],
+            [2, 2],
+        ];
+    } else if (type === 'L') {
+        return [
+            [0, 3, 0],
+            [0, 3, 0],
+            [0, 3, 3],
+        ];
+    } else if (type === 'J') {
+        return [
+            [0, 4, 0],
+            [0, 4, 0],
+            [4, 4, 0],
+        ];
+    } else if (type === 'I') {
+        return [
+            [0, 5, 0, 0],
+            [0, 5, 0, 0],
+            [0, 5, 0, 0],
+            [0, 5, 0, 0],
+        ];
+    } else if (type === 'S') {
+        return [
+            [0, 6, 6],
+            [6, 6, 0],
+            [0, 0, 0],
+        ];
+    } else if (type === 'Z') {
+        return [
+            [7, 7, 0],
+            [0, 7, 7],
+            [0, 0, 0],
+        ];
+    }
+}
+
+export default class Player {
+    constructor(tetris) {
         this.DROP_SLOW = 1000;
         this.DROP_FAST = 50;
 
-        this.tetris = tetris;
+        this.tetris = tetris
         this.arena = tetris.arena;
 
         this.dropCounter = 0;
@@ -18,28 +62,26 @@ class Player
         this.reset();
     }
 
-    drop ()
-    {
+    drop () {
         this.pos.y++;
         if (this.arena.collide(this)) {
             this.pos.y--;
             this.arena.merge(this);
             this.reset();
             this.score += this.arena.sweep();
-            this.tetris.updateScore(this.score);
+            this.tetris.updateScore(this.score)
         }
         this.dropCounter = 0;
     }
 
-    move(dir){
+    move (dir) {
         this.pos.x += dir;
         if (this.arena.collide(this)) {
             this.pos.x -= dir;
         }
     }
 
-    reset()
-    {
+    reset () {
         const pieces = 'ILJOTSZ';
         this.matrix = createPiece(pieces[pieces.length * Math.random() | 0]);
         this.pos.y = 0;
@@ -48,7 +90,7 @@ class Player
         if (this.arena.collide(this)) {
             this.arena.clear()
             this.score = 0;
-            updateScore();
+            this.tetris.updateScore(this.score)
         }
     }
 
@@ -67,8 +109,7 @@ class Player
         }
     }
 
-    _rotateMatrix(matrix, dir)
-    {
+    _rotateMatrix(matrix, dir) {
         for (let y = 0; y < matrix.length; ++y) {
             for (let x = 0; x < y; ++x){
                 [
@@ -88,8 +129,7 @@ class Player
         }
     }
 
-    update(deltaTime)
-    {
+    update(deltaTime) {
         this.dropCounter += deltaTime;
         if (this.dropCounter > this.dropInterval) {
             this.drop();
